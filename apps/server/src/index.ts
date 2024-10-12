cf49d27f837db48f2cdb2808fd35f2d53933bcbc
OfirTeneJuno <ofir@junojourney.com>
2024-10-12 11:13:05 +0300

    server changes

--- apps/server/src/index.ts
+++ apps/server/src/index.ts
@@ -1,15 +1,16 @@
-import express, { Request, Response } from 'express';
-import { MySharedType } from '@shared/types';
+import express, { Request, Response } from "express";
+import { MySharedType } from "@shared/types";
 
 const app = express();
 const port = 3000;
 const x: MySharedType = {
-  id: '1',
-  name: 'John Doe',
+  id: "1",
+  name: "Ofir Tene",
 };
 // Basic route to test the server
-app.get('/', (req: Request, res: Response) => {
-  res.send('Hello, TypeScript with Express!');
+app.get("/api", (req: Request, res: Response) => {
+  res.set("content-type", "application/json");
+  res.send(x);
 });
 
 // Start the server
